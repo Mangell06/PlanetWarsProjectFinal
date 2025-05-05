1df9442aacca343eb470ad95baf628b7650c8050
MLS-2004 <marclosada2004@gmail.com>
2025-05-05 16:32:56 +0200

    Principio de pagina y css

---
+++ js_button_menu.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");  // Obtén el menú
+    menu.classList.toggle("show");  // Añade o elimina la clase 'show' para mostrar/ocultar el menú
+}
+

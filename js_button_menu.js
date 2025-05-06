2756c34ab09b11b2d32cf48693a1f22096dbda92
MLS-2004 <marclosada2004@gmail.com>
2025-05-06 21:20:59 +0200

    dia2.2
    mejora del punto de izquierda
    de momento todo el contenido de tutorial
    mejoras en el css
    documentos de prueba de descarga
    mejoras generales
    apartado de descarga

--- js_button_menu.js
+++ js_button_menu.js
@@ -3,3 +3,7 @@ function toggleMenu() {
     menu.classList.toggle("show");  // Añade o elimina la clase 'show' para mostrar/ocultar el menú
 }
 
+function toggleDown() {
+    var download = document.getElementById("download");  // Obtén el menú de descarga
+    download.classList.toggle("show");  // Añade o elimina la clase 'show' para mostrar/ocultar el menú de descarga
+}
